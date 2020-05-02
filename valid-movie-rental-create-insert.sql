af2e83dc572b29a02a449cec0b034128a92abc74
Guido Mantilla <guido.mantilla@yahoo.com>
2020-05-02 11:28:57 -0500

    Fixes Commit

--- valid-movie-rental-create-insert.sql
+++ valid-movie-rental-create-insert.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE IF NOT EXISTS `valid-movie-rental` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `valid-movie-rental`;
 -- MySQL dump 10.13  Distrib 8.0.16, for Linux (x86_64)
 --
 -- Host: localhost    Database: valid-movie-rental
