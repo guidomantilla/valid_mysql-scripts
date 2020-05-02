a2f42315046fc15cd26b6d012ac6de171764e8c7
Guido Mantilla <guido.mantilla@yahoo.com>
2020-05-02 13:52:25 -0500

    Fixes Commit

--- build.sh
+++ build.sh
@@ -1,21 +1,29 @@
 PARENT_NAME=$1
 if [ -z "$PARENT_NAME" ]; then
-    APP_NAME=valid_mysql
+    APP_NAME=valid-mysql
 else
     APP_NAME="$PARENT_NAME"
 fi
 
 PARENT_PORT=$2
 if [ -z "$PARENT_PORT" ]; then
-    PORT=3306
+    PORT=3308
 else
     PORT="$PARENT_PORT"
+fi
+
+PARENT_PASSWORD=$3
+if [ -z "$PARENT_PASSWORD" ]; then
+    PASSWORD="v4l1d-gu1d0-m4nt*"
+else
+    PASSWORD="$PARENT_PASSWORD"
 fi
 
 
 docker build . -t "$APP_NAME" \
 && docker container rm --force "$APP_NAME" 
    docker container run --detach --restart always \
-                        --network valid_network \
+                        --network valid-network \
+                        --env MYSQL_ROOT_PASSWORD="$PASSWORD" \
                         --publish "$PORT":3306 \
                         --name "$APP_NAME" "$APP_NAME"
